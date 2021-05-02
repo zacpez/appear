b0c170893bf2d94cb91890f6d28b90acccc7fd9e
zacpez <zacpez@gmail.com>
2021-05-02 13:36:35 -0700

    add: github actions

--- appear/commands/build.py
+++ appear/commands/build.py
@@ -1,9 +1,20 @@
 import click
+import os
+
+
+def load_appear_config():
+    if os.path.isdir('.appear') is False:
+        click.echo(click.style('No appear configuration available, generate one with appear init', bg='red'))
+        return False
+    return dict()
 
 
 def generate_app():
     """Creates a summary report for hte generated builds."""
-    print('build')
+    click.echo(click.style(f'Building app in {os.getcwd()}', bg='green'))
+    config = load_appear_config()
+    if config is False:
+        exit(1)
 
 
 def generate_summary():
